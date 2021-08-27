803b0e1edffc74664b475d1b8e53e46847bb4639
uptickskat <89293992+uptickskat@users.noreply.github.com>
2021-08-27 04:26:19 -0400

    Update drugs.lua

--- drugs.lua
+++ drugs.lua
@@ -26,44 +26,75 @@ end
 
 local function PrintSolution()
 	print''
-	print("Total Mass:",total,"g")
+	print("Total Mass:",total,"g","\n")
 	print("Solutions Used:")
 	for i,v in pairs(drugs) do
-		print(i,'-',drugs2[i]..'g',math.floor(((drugs2[i])/total)*10000+.5)/100,"%")
-	end
-	print''
-	print('Chemical Breakdown: '..(total*1000)..' mg')
-	for i,v in pairs(contents) do
-		if not (v==0) then
-			print(i,'-',(v*10)..'mg')
+		if drugs2[i] > 0 then
+			print(i,'-',drugs2[i]..'g',math.floor(((drugs2[i])/total)*10000+.5)/100,"%")
 		end
 	end
 	print''
 	print('Percent Breakdown: \n')
-	for i,v in pairs(drugs) do
-		for dname, dvalue in pairs(v) do
-			if not (contents[dname] == 0) then
-				contents[dname] = math.floor((contents[dname]/(total))*100+.5)/100 
-				print(dname,contents[dname]..'%')
-			end
+	print("THC(thca * .877 + thc)")
+	print("CBD(cbda * .877 + cbd\n")
+	if contents['thca'] then
+		contents['d9thc'] += contents['thca']*.877;
+		contents['thca']=0
+	end
+	if contents['cbda'] then
+		contents['cbd'] += contents['cbda']*.877;
+		contents['cbda']=0
+	end
+	
+	for i,v in pairs(contents) do
+		if v>0 then
+			print(i,math.floor((v/(total))*100+.5)/100,"%" )
 		end
 	end
 	
 end
 
-AddToSolution("Distillate",{
-d8thc = 93.43,
-
+AddToSolution("d8-7-10-21",{
+	d8thc = 93.01,
+	d9thc = .15,
+	thca = 1,
+})
+AddToSolution("d8-Laughing_buddha",{
+	d8thc = 94.900,
+})
+AddToSolution("freshbro",{
+d8thc = 75.072,
 });
 AddToSolution("BSCBD",{
-cbd = 79.10,
-cbn = 0.15,
-cbg = 4.52,
-thcv = 0.06,
-cbdv = 1.74,
+cbd = 88.26,
+cbn = .22,
+cbg = 7.16,
+thcv = .08,
+cbdv = .57
 });
 
-Add("Distillate",1)
-Add("BSCBD",2.5)
+AddToSolution("BS-7-16-21",{
+cbd = 90.3854,
+cbdv = 1.6097,
+cbg = 4.0689,
+cbga = 0.4375,
+})
 
+AddToSolution("BS-3-15-21",{
+cbd = 77.5565,
+cbdv = 4.6787,
+cbg = 3.4906,
+cbga = 0.4892,
+})
+AddToSolution("BS-4-06-21",{
+cbd = 90.8088,
+cbg = 7.5748,
+})
+AddToSolution("d9disty",{
+
+d9thc = 92.3,})
+
+
+Add("d8-7-10-21",3)
+Add("BSCBD",6)
 PrintSolution()
