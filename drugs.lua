f7661e600aee4cbe1f609ab13e87cb9f7bee4ec7
uptickskat <89293992+uptickskat@users.noreply.github.com>
2021-10-24 16:45:28 -0400

    Update drugs.lua

--- drugs.lua
+++ drugs.lua
@@ -1,100 +1,150 @@
-local total = 0;
-local drugs = {};
-local drugs2 = {};
-local contents = {};
+-- CMD C:\Users\whatd\Documents\Lua programs>lua newdrug.lua
+-- Drug Calculator
+-- v2.0
+--  ~ Solution Percents -ne
+--  + Solution Cost ~ye
+--  + Active solution / mg ~eh
+--  + In-Active solution / mg ~eh
+--  + Recommended price for resale ~yeh
+--  ? Solution Conversions (i.e. Batchs w/ drops) ~ye
+--		~ dosage / drop and mass used into total dosage ~ne
+--
+--
+--
 
-local function AddToSolution(Name,table)
-	drugs[Name]=table;
-	drugs2[Name] = 0
-	for i,v in pairs(table) do
-		if contents[i] then
-			contents[i] += v;
-		end
-	end
+-- to do:
+-- match string length and pixel size for dose/mg to line up in middle
+math.randomseed(os.time());
+function d(...)
+	print(...)
 end
-local function Add(Solution,Amount)
-	drugs2[Solution] += Amount;
-	total += Amount;
-	for i,v in pairs(drugs[Solution]) do
-		if contents[i] then
-			contents[i] += drugs[Solution][i]*Amount
-		else
-			contents[i] = drugs[Solution][i]*Amount
-		end
-	end
+function ToTincture(Drog,ml)
+	
+	return {Drop = {},Dropper = {}}
 end
 
-local function PrintSolution()
-	print''
-	print("Total Mass:",total,"g","\n")
-	print("Solutions Used:")
-	for i,v in pairs(drugs) do
-		if drugs2[i] > 0 then
-			print(i,'-',drugs2[i]..'g',math.floor(((drugs2[i])/total)*10000+.5)/100,"%")
-		end
-	end
-	print''
-	print('Percent Breakdown: \n')
-	print("THC(thca * .877 + thc)")
-	print("CBD(cbda * .877 + cbd\n")
-	if contents['thca'] then
-		contents['d9thc'] += contents['thca']*.877;
-		contents['thca']=0
-	end
-	if contents['cbda'] then
-		contents['cbd'] += contents['cbda']*.877;
-		contents['cbda']=0
+function RandomString_Gen(Length, List)
+	local Batch_Sigs = {'DEX'};
+	math.randomseed(os.time());
+	local ret = '';
+	for i=1,Length do
+		local dub = math.random(1,string.len(List));
+		ret = ret .. string.upper(List:sub(dub,dub));
 	end
 	
-	for i,v in pairs(contents) do
-		if v>0 then
-			print(i,math.floor((v/(total))*100+.5)/100,"%" )
-		end
-	end
-	
+	return (Batch_Sigs[math.random(1,2)]..' - '..ret)
 end
+local solution = {
+	print('Batch:',RandomString_Gen(8,'jvxoudrtyqcmk'));
+	v = 0; -- ml/g
+	cost = 0;
+	startup = 0;
+	costs = {};
+	Content = {};
+	SetCost = function(self,cost)
+		self.cost = cost;
+		self.startup = cost;
+	end,
+	Add = function(solution,Name, Cost, Proportions, Volume)
+		solution.cost = solution.cost + (Cost*(Volume)); -- .15 for lost distillate
+		solution.v = solution.v + Volume;
+		for index,value in pairs(Proportions) do
+			solution.costs[index] = (Cost*Volume)*(Proportions[index]/100); -- = price per ml
+			if solution.Content[index] then
+				solution.Content[index] = solution.Content[index] + (value * (Volume*10));
+			else
+				solution.Content[index] = value * Volume*10;
+			end
+		end;
+	end,
+	Print = function(self,	arg)
+		for i,v in pairs(self.costs) do
+			self.costs[i] = (math.floor((v / self.v)*100+.5)/100);
+		end
+		if arg == 1 then -- Print Contents
+			print"\nContents\n"
+			for i,v in pairs(self.Content) do
+				print(i,v.." mg");
+			end
+			print''
+		elseif arg == 2 then -- Print Contents Cart
+			print"\nCart Contents (mg/ml)\n"
+			for i,v in pairs(self.Content) do
+				print(i,(math.floor((v/self.v)*100)/100).." mg");
+			end
+			print''
+		elseif arg == 3 then -- Print Contents Tincture
+			print"\nTincture Contents  mg(ml/drop)\n"
+			for i,v in pairs(self.Content) do
+				if i ~= 'Filler' then
+					print('',i,(math.floor((v/self.v)*10)/10)..'/'..(math.floor(((v/self.v)*10)*.05+.5)/10));
+				end
+			end
+			print''
+		end
+		print'================'
+		print('Startup Cost  =  '..self.startup, '$');
+		print('Total Cost  =   '..math.floor((self.cost)*10)/10,'$\n================\n');
+		print'Suggested Retail:'
+		local markup = math.floor((self.cost+.599)*2.5*10)/10;
+		if (math.floor(markup/10+.5)*10 > markup) then
+			markup = math.floor(markup/10+.5)*10; 
+		else -- Yes I markup after I markup. part of markups are for lost distillate in transfer, incorrect ratios, and time
+			markup = (math.floor(markup/10)*10+5);
+		end
+		print(markup,'$')
+		print'\n================'
+		
+	end
+}
 
-AddToSolution("d8-7-10-21",{
-	d8thc = 93.01,
-	d9thc = .15,
-	thca = 1,
+local Solution = setmetatable(solution,{
+	__index = function(self,prop)
+        if not self.Content[prop] or (self.Content[prop] == 0) then
+            return 0
+        else
+		 -- Name In_Solution / 1g(ml)    name_Percent
+			print('',prop,math.floor(self.Content[prop])..' / '..math.floor((self.Content[prop]/self.v)*100)/100,(math.floor(((self.Content[prop]*100)/self.v)+.5)/1000)..'%')
+            return print('','',(math.floor(self.costs[prop]/self.v*100000)/100000)..'$');
+        end
+    end,
 })
-AddToSolution("d8-Laughing_buddha",{
-	d8thc = 94.900,
-})
-AddToSolution("freshbro",{
-d8thc = 75.072,
-});
-AddToSolution("BSCBD",{
-cbd = 88.26,
-cbn = .22,
-cbg = 7.16,
-thcv = .08,
-cbdv = .57
-});
-
-AddToSolution("BS-7-16-21",{
-cbd = 90.3854,
-cbdv = 1.6097,
-cbg = 4.0689,
-cbga = 0.4375,
-})
-
-AddToSolution("BS-3-15-21",{
-cbd = 77.5565,
-cbdv = 4.6787,
-cbg = 3.4906,
-cbga = 0.4892,
-})
-AddToSolution("BS-4-06-21",{
-cbd = 90.8088,
-cbg = 7.5748,
-})
-AddToSolution("d9disty",{
-
-d9thc = 92.3,})
+function MajorNoids()
+	local a;
+	print' Active Contents:\n'
+	a = Solution['d9thc'];
+	a = Solution['d9thca'];
+	a = Solution['d8thc'];
+	a = Solution['d8thco'];
+	a = Solution['d9thco'];
+	a = Solution['d10thc'];
+	a = Solution['hhc-9s'];
+	a = Solution['hhc-9r'];
+	a = Solution['hhc-9u'];
+	print' Inactive Contents:\n'
+	a = Solution.cbd;
+	a = Solution.cbdv
+	a = Solution.cbn;
+	a = Solution.cbg;
+	a = Solution.cbt;
+	a = Solution.cbc;
+	a = Solution.cbe;
+	a = Solution.cbl;
+	a = Solution.terps;
+end
+Solution:SetCost(3.6); -- Set starting cost (i.e. Cartridge cost per(1), or Tincture bottle glass per(1)
+Solution:Add('Terps',20,{['terps']=100},.05);
+--Solution:Add('GVB-Biopharma Full Spectrum CBD Distillate',2.7,{['cbd']=86.5,['d9thc']=.15,['cbdv']=.7,['cbt']=2.3},2);
+Solution:Add('Loud house hemp Delta-8',1.6,{['d8thc']=93,},.95)
+--Solution:Add('70% Ethanol',.08,{Filler=100},54);
+Solution:Print(2);
+MajorNoids()
+--print(Solution.cbd)
 
 
-Add("d8-7-10-21",3)
-Add("BSCBD",6)
-PrintSolution()
+
+
+
+ -- .8g d8	
+ -- .15g FSE
+ -- .05g terps
